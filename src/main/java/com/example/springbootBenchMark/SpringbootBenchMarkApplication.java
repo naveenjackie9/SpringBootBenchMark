9ff60d9eb1cde9265841aba70a834480272ab227
Naveen Mati Doddabasappa <nmdoddabasappa@falabella.cl>
2022-10-26 13:07:52 +0530

    Booting up project: Project Checkin

---
+++ src/main/java/com/example/springbootBenchMark/SpringbootBenchMarkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootBenchMark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBenchMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBenchMarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootBenchMark/SpringbootBenchMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootBenchMark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBenchMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
